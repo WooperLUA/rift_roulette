1038accadae9b6a443fe7a80ae499480337e28e8
Pons Emilien <emilien.pons@etu.iut-tlse3.fr>
2025-08-08 21:55:18 +0200

    fixed the fact that github pages is so retarted it cant serve .json

--- index.js
+++ index.js
@@ -1,26 +1,10 @@
-let champions;
-let lanes;
-let builds;
+import * as audio_handler from './audio_handler.js'
 
-fetch('../data/champions.json')
-    .then(response => response.json())
-    .then(data => {
-        champions = data;
-    })
+// For some obscure reasons GitHub cant serve .json so i guess it is gonna be js
+import {champions} from "./data/github_json_champions.js"
+import {lanes} from "./data/github_json_lanes.js"
+import {build_types} from "./data/github_json_build_types.js"
 
-fetch('../data/lanes.json')
-    .then(response => response.json())
-    .then(data => {
-        lanes = data;
-    })
-
-fetch('../data/build_types.json')
-    .then(response => response.json())
-    .then(data => {
-        builds = data;
-    })
-
-console.log(champions);
 
 const champSlot = document.getElementById('champSlot');
 const laneSlot = document.getElementById('laneSlot');
@@ -43,6 +27,7 @@ function spinSlot(slotElem, items, duration)
             if (elapsed >= duration) {
                 clearInterval(interval);
                 // Final pick
+                audio_handler.play_sound('funny_taunt', 0.2)
                 const finalPick = getRandom(items);
                 slotElem.textContent = finalPick;
                 resolve(finalPick);
@@ -55,9 +40,11 @@ spinBtn.addEventListener('click', async () =>
 {
     spinBtn.disabled = true;
 
+    audio_handler.play_sound('very_humoristic_drum_roll', 0.4)
+
     const champResult = spinSlot(champSlot, champions, 2000);
     const laneResult = spinSlot(laneSlot, lanes, 2500);
-    const buildResult = spinSlot(buildSlot, builds, 3000);
+    const buildResult = spinSlot(buildSlot, build_types, 3000);
 
     const [champ, lane, build] = await Promise.all([champResult, laneResult, buildResult]);
 
